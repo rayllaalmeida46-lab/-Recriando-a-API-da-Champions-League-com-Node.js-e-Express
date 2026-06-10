5cbd1ca05eb1bac746de11eec92c676d9c606eb9
rayllaalmeida46-lab <rayllaalmeida46@gmail.com>
2026-06-10 15:18:12 -0300

    Create players.js with player data
    Added an array of player objects with id, name, clubId, and position.

---
+++ src/data/src/data/players.js
@@ -0,0 +1,22 @@
+const players = [
+  {
+    id: 1,
+    name: "Vinicius Junior",
+    clubId: 1,
+    position: "Forward"
+  },
+  {
+    id: 2,
+    name: "Erling Haaland",
+    clubId: 2,
+    position: "Forward"
+  },
+  {
+    id: 3,
+    name: "Harry Kane",
+    clubId: 3,
+    position: "Forward"
+  }
+];
+
+module.exports = players;
