01c6a41dc059f0e87ccbcf9c05638dc273ac64bb
rayllaalmeida46-lab <rayllaalmeida46@gmail.com>
2026-06-10 15:16:44 -0300

    Create clubs.js with club data
    Add initial data for football clubs with titles.

---
+++ src/data/clubs.js
@@ -0,0 +1,22 @@
+const clubs = [
+  {
+    id: 1,
+    name: "Real Madrid",
+    country: "Spain",
+    titles: 15
+  },
+  {
+    id: 2,
+    name: "Manchester City",
+    country: "England",
+    titles: 1
+  },
+  {
+    id: 3,
+    name: "Bayern Munich",
+    country: "Germany",
+    titles: 6
+  }
+];
+
+module.exports = clubs;
